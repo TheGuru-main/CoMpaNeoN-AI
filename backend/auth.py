715250cb24384eb74d997346277763bbff77fae8
TheGuru-main <idrisdfirst@gmail.com>
2026-08-24 23:35:34 +0100

    Update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -20,10 +20,13 @@ def get_db():
         db.close()
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(pwd_bytes, salt).decode('utf-8')
 
-def verify_password(plain: str, hashed: str) -> bool:
-    return pwd_context.verify(plain, hashed)
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
+
 
 def create_access_token(user_id: str):
     expire = datetime.utcnow() + timedelta(days=7)
